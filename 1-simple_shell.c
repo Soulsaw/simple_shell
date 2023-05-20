5c471d2d711ca7f83e6d6c61bf14c1eba1c5aec3
Soulsaw <ssawadogo292@gmail.com>
2023-05-20 17:01:22 +0000

    solved the task 1 on the simple shell project

---
+++ 1-simple_shell.c
@@ -0,0 +1,47 @@
+#include "main.h"
+/**
+ * main - The entry point of program
+ *
+ * Return: Always 0.
+ *
+ *
+ */
+int main(void)
+{
+	char *argv[] = {NULL, NULL};
+	char *cmd = NULL;
+	size_t len;
+	int i = 0, cpt = 0, status;
+	pid_t myPid;
+
+	while (1)
+	{
+		myPid = fork();
+		if (myPid == 0)
+		{
+			printf("#cisfun$ ");
+			if (getline(&cmd, &len, stdin) == -1)
+			{
+				perror(__FILE__);
+			}
+			while (cmd[i] != '\0')
+			{
+				cpt++;
+				i++;
+			}
+			cmd[cpt - 1] = '\0';
+			argv[0] = cmd;
+			if (execve(argv[0], argv, NULL) == -1)
+			{
+				perror(__FILE__);
+			}
+		}
+		else
+		{
+			wait(&status); /** The parent process wait
+					* then childreen process terminate
+					*/
+		}
+	}
+	return (0);
+}
---
+++ main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <unistd.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+#endif /* MAIN_H */
