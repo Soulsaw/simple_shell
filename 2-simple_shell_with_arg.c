455877678cededf1acfa86808b4680cd7321eb4f
Soulsaw <ssawadogo292@gmail.com>
2023-05-22 16:26:43 +0000

    The simple shell task with arguments and path

---
+++ 2-simple_shell_with_arg.c
@@ -0,0 +1,57 @@
+#include "main.h"
+#include <stdlib.h>
+#include <signal.h>
+#include <string.h>
+/**
+ * main - The entry point of program
+ *
+ * Return: Always 0.
+ *
+ *
+ */
+int main(void)
+{
+	char *argv[] = {NULL, NULL, NULL};
+	char *cmd = NULL, *str1, *token;
+	size_t len;
+	int i = 0, cpt = 0, status, j;
+	pid_t myPid;
+
+	while (1)
+	{
+		myPid = fork();
+		if (myPid == 0)
+		{
+			printf("#cisfun$ ");
+			if (getline(&cmd, &len, stdin) == -1)
+			{
+				perror(__FILE__);
+			}
+			while (cmd[i] != '\0')
+			{
+				cpt++;
+				i++;
+			}
+			cmd[cpt - 1] = '\0';
+			for (j = 0, str1 = cmd; ; j++, str1 = NULL)
+			{
+				token = strtok(str1, " ");
+				if (token == NULL)
+					break;
+				argv[j] = token;
+			}
+			argv[2] = NULL;
+			if (execve(argv[0], argv, NULL) == -1)
+			{
+				perror(__FILE__);
+			}
+		}
+		else
+		{
+			wait(&status); /** The parent process wait
+					* then childreen process terminate
+					*/
+		}
+	}
+	return (0);
+}
